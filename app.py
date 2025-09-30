ebbbe810fb57f4a34886ce557d4b6129af41f577
Sakthivel <100959188+I-am-Sakthivel@users.noreply.github.com>
2025-09-30 19:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def login():
         password = request.form.get("password")
         u,p=os.getenv('u'),os.getenv('p')
         if u==username and p==password:
-            return redirect('/6e686d5bd5cb58ae1c865206be71fc3560ea6fa8f32da3ed25bf99cda104ee1d')
+            return redirect(os.getenv('index'))
         else:
             return render_template("failed_login.html")
     return render_template("login.html")
 
-@app.route('/6e686d5bd5cb58ae1c865206be71fc3560ea6fa8f32da3ed25bf99cda104ee1d')
+@app.route(os.getenv('index'))
 def ind():
     return render_template('index.html')
 
@@ -23,4 +23,5 @@ def ind():
 if __name__ == '__main__':
 
     app.run()
+
 
